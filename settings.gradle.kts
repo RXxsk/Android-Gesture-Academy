272886be922ed11010c62e7d4cdf5656731c33a7
RXx <60585034+RXxsk@users.noreply.github.com>
2026-03-18 21:00:00 -0600

    Configure plugin and dependency management in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,21 @@
+kotlin
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
+rootProject.name = "Android-Gesture-Academy"
+include(":library:gesture-core")
+//include(":library:accessibility") // Lo agregaremos después
+//include(":library:samples") // Lo agregaremos después
+//include(":plugin") // Lo agregaremos después
